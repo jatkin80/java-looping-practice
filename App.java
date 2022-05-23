d87ae85d04ff66736f747518cdcae03dbaaa36c3
Julie <jatkin80@ford.com>
2022-05-23 16:07:15 -0500

    complete exercise

--- App.java
+++ App.java
@@ -6,9 +6,26 @@ class App {
   }
 
   private static void forLoop() {
+
+    for (int i=10; i>0; i--)
+    {
+    System.out.println(i);
   }
+  System.out.println("Launch");
+}
   private static void whileLoop() {
+    int i=0;
+    while (i<5){
+    System.out.println("I love Java");
+    i++;
+  }
   }
   private static void doWhileLoop() {
+    int i=0;
+    do {
+    System.out.println("I love Java");
+    i++;
   }
+ while (i<5);
+}
 }
