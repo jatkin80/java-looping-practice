d9d898a1f971253d032a951b9b8d6091aef75402
Julie <jatkin80@ford.com>
2022-05-25 09:44:19 -0500

    correct formatting

--- App.java
+++ App.java
@@ -1,31 +1,30 @@
 class App {
-  public static void main(String args[]){
+  public static void main(String args[]) {
     App.forLoop();
     App.whileLoop();
     App.doWhileLoop();
   }
 
   private static void forLoop() {
+    for (int i = 10; i > 0; i--) {
+      System.out.println(i);
+    }
+    System.out.println("Launch");
+  }
 
-    for (int i=10; i>0; i--)
-    {
-    System.out.println(i);
-  }
-  System.out.println("Launch");
-}
   private static void whileLoop() {
-    int i=0;
-    while (i<5){
-    System.out.println("I love Java");
-    i++;
+    int i = 0;
+    while (i < 5) {
+      System.out.println("I love Java");
+      i++;
+    }
   }
-  }
+
   private static void doWhileLoop() {
-    int i=0;
+    int i = 0;
     do {
-    System.out.println("I love Java");
-    i++;
+      System.out.println("I love Java");
+      i++;
+    } while (i < 5);
   }
- while (i<5);
-}
 }
